0ee2945c7115511bc53b575925f63c09ba6ebe63
Ashutosh Nath Agarwal <ash7594@gmail.com>
2016-03-31 03:45:18 +0530

    arrow interaction

--- wheel.js
+++ wheel.js
@@ -5,21 +5,40 @@ var windowWidth = window.innerWidth * 0.98,
 	windowHeight = window.innerHeight * 0.96,
 	windowCX = windowWidth/2,
 	windowCY = windowHeight/2,
-	windowA = 0;
+	windowA = 0,
+	wheelRad = 200;
+
+var pivotInverseRad = 10;
+
+var pointerRad = 50,
+	pointerSectors = 8,
+	pointerInitialAngle = 90;
 
 canvas.width = windowWidth;
 canvas.height = windowHeight;
 
-function polygonPar() {
+function polygonPar(sectors) {
     this.x = windowCX;
     this.y = windowCY;
-    this.r = 200; //5 * entity.r;
+    this.r = wheelRad; //5 * entity.r;
     this.c = "rgba(0,0,0,1)"; //lineColour
     this.w = 3;  //linewidth
-	this.sectors = 6;
+	this.sectors = sectors;
 }
 
-var wheel = new polygonPar();
+function pointerClass() {
+	this.x = windowCX;
+	this.y = windowCY - wheelRad - pointerRad + pivotInverseRad;
+	this.r = pointerRad;
+	this.sectors = pointerSectors;
+	this.innerAng = (360 / pointerSectors) / 2;
+	this.a = pointerInitialAngle;
+	this.angMoved = 0;
+	this.c = "rgb(255,150,0)";
+}
+
+var wheel = new polygonPar(6),
+	pointer = new pointerClass();
 
 function polygonRender() {
 	for (var j=0; j<wheel.sectors; j++) {
@@ -37,12 +56,44 @@ function polygonRender() {
 		ctx.fill();
 		ctx.closePath();
 	}
+
+	for (var j=0; j<wheel.sectors; j++) {
+		ctx.beginPath();
+		ctx.arc(
+				windowCX + (wheel.r - pivotInverseRad) * Math.cos((windowA + j * 360 / wheel.sectors) * Math.PI / 180),
+				windowCY + (wheel.r - pivotInverseRad) * Math.sin((windowA + j * 360 / wheel.sectors) * Math.PI / 180),
+				(wheel.r - pivotInverseRad) / 35,
+				0, 2 * Math.PI);
+		ctx.fillStyle = "rgb(255,0,0)";
+		ctx.fill();
+		ctx.closePath();	
+	}
+}
+
+function collision() {
+	pointer.a--;
+	pointer.angMoved++;
+	if (pointer.r - (pointer.r * Math.cos(pointer.angMoved * Math.PI / 180)) >= (wheel.r - pivotInverseRad)/35) {
+		
+	}
+}
+
+function pointerRender() {
+	ctx.beginPath();
+	ctx.moveTo(pointer.x, pointer.y);
+	ctx.lineTo(pointer.x + pointer.r * Math.cos(pointer.a * Math.PI / 180), pointer.y + pointer.r * Math.sin(pointer.a * Math.PI / 180));
+	ctx.strokeStyle = pointer.c;
+	ctx.stroke();
+	ctx.closePath();
 }
 
 function render() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     polygonRender();
-    windowA++;
+	pointerRender();
+    windowA = (windowA+1)%360;
+	if ((windowA + (360 / wheel.sectors) / 2) % (360 / wheel.sectors) == 0)
+		collision();		
 }
 
 function variableBar() {
